c749fd4ea318be70e9ca613d37e07df530045cb6
numbat70 <numbat70@icloud.com>
2018-10-23 17:17:08 +0100

    mka18

--- HashName/js/calc.js
+++ HashName/js/calc.js
@@ -1,20 +1,3 @@
 $("#buttonCalc").click(function() {
-
   // alert("Start");
-
-// minmin
-// min
-//    $("#minmin").val(varminmin);
-
-// plus
-// plusplus
-
-// alert(VOG);
-// alert(VOG);
-// alert(VOG);
-
-//	DDP_display = parseInt(DDP);
-//	DDP_display = DDP_display.toLocaleString();
-//	$("#DDP2").val(DDP_display);
-
 });
