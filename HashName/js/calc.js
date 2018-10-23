9f5832894f5d92a524ae04147d9e67ed41b54732
numbat70 <numbat70@icloud.com>
2018-10-23 17:05:20 +0100

    mka6

--- HashName/js/calc.js
+++ HashName/js/calc.js
@@ -7,11 +7,11 @@ $("#buttonCalc").click(function() {
     var varminmin = "Mooooo" ;
     //FOB = parseInt(FOB);
 	//FOB = FOB.toLocaleString();
-    $("#minmin").val(varminmin);
+//    $("#minmin").val(varminmin);
 
 //plus
 alert($(#varRAW).val());
-  var TC=parseInt($("#TC").val());
+//  var TC=parseInt($("#TC").val());
 
 
 //plusplus
@@ -21,28 +21,19 @@ alert($(#varRAW).val());
 //alert(VOG);
 //alert(VOG);
 
-//Plus and Minus 5%
-    $("#VOG2").val(VOG);
-    $("#VOG3").val(VOG);
-    $("#VOG4").val(VOG);
 
-var FOBplus = ((COH + COC + HC)*1.05) + VOG;
-var FOBminus = ((COH + COC + HC)*0.95) + VOG;
-
-    FOBplus_display = parseInt(FOBplus);
-	FOBplus_display = FOBplus_display.toLocaleString();
-    $("#FOBplus").val(FOBplus_display);
+//	FOBplus_display = FOBplus_display.toLocaleString();
+//    $("#FOBplus").val(FOBplus_display);
 
 
 
-	DDPminus_display = parseInt(DDPminus);
-	DDPminus_display = DDPminus_display.toLocaleString();
-    $("#DDPminus").val(DDPminus_display);
+//	DDPminus_display = DDPminus_display.toLocaleString();
+//    $("#DDPminus").val(DDPminus_display);
 
 	//Already done higher up
-	DDP_display = parseInt(DDP);
-	DDP_display = DDP_display.toLocaleString();
-	$("#DDP2").val(DDP_display);
+//	DDP_display = parseInt(DDP);
+//	DDP_display = DDP_display.toLocaleString();
+//	$("#DDP2").val(DDP_display);
 });
 
 //DDP = parseInt(DDP);
