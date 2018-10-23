a99221c72af19ec361502e4dc2455b05a8832698
numbat70 <numbat70@icloud.com>
2018-10-23 17:11:28 +0100

    mka12b

--- HashName/js/calc.js
+++ HashName/js/calc.js
@@ -2,17 +2,12 @@ $("#buttonCalc").click(function() {
 
   alert("Start");
 
-
 //minmin
 
 //min
-    //FOB = parseInt(FOB);
-	//FOB = FOB.toLocaleString();
 //    $("#minmin").val(varminmin);
 
 //plus
-//alert($(#varRAW).val());
-//  var TC=parseInt($("#TC").val());
 
 //plusplus
 
@@ -21,17 +16,8 @@ $("#buttonCalc").click(function() {
 //alert(VOG);
 //alert(VOG);
 
-//	FOBplus_display = FOBplus_display.toLocaleString();
-//    $("#minmin").val(varminmin);
-
-//	DDPminus_display = DDPminus_display.toLocaleString();
-//    $("#DDPminus").val(DDPminus_display);
-
-	//Already done higher up
 //	DDP_display = parseInt(DDP);
 //	DDP_display = DDP_display.toLocaleString();
 //	$("#DDP2").val(DDP_display);
-});
 
-//DDP = parseInt(DDP);
-//DDP = DDP.toLocaleString();
+});
